331a91092545499418e533eeb87ef21c97bba7d5
Konradloehrke <Konrad.Loehrke@msg.group>
2025-07-10 16:22:26 +0200

    Testfälle vom 10.07.2025
    Testfälle Level2  16-21

---
+++ cypress/e2e/Level2/Testfall23 book suchen und Ergebnisse prüfen .cy .js
@@ -0,0 +1,10 @@
+describe('books suchen und ergebnisse überprüfen', () => {
+  it('soll die Seite Bücher laden und  ', () => {
+
+cy.visit('https://demowebshop.tricentis.com')
+cy.contains('books').click();
+cy.contains('books').should('be.visible');
+cy.get('.product-item').should('have.length', 3);
+    
+  });
+});
---
+++ cypress/e2e/Level2/Testfall24 computer suchen und Ergebnisse prüfen. cy.js
@@ -0,0 +1,9 @@
+describe('comuter suchen und ergebnisse überprüfen', () => {
+  it('Sollte die Seite Computer laden und die Ergebnisse prüfen', () => {
+
+
+
+
+    
+  });
+});
