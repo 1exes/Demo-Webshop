89219f4eee6f487e6cf1ef864ededeb65dde7666
Konradloehrke <Konrad.Loehrke@msg.group>
2025-07-11 08:59:11 +0200

    Testfälle 23-25

--- cypress/e2e/Level2/Testfall24 computer suchen und Ergebnisse prüfen. cy.js
+++ cypress/e2e/Level2/Testfall24 computer suchen und Ergebnisse prüfen. cy.js
@@ -2,7 +2,10 @@ describe('comuter suchen und ergebnisse überprüfen', () => {
   it('Sollte die Seite Computer laden und die Ergebnisse prüfen', () => {
 
 
-
+cy.visit('https://demowebshop.tricentis.com')
+cy.contains('computers').click();
+cy.contains('computers').should('be.visible');
+cy.get('.product-item').should('have.length', 2);
 
     
   });
--- cypress/e2e/Level2/Testfall25 nicht exsistierendes Produkt cy.js
+++ cypress/e2e/Level2/Testfall25 nicht exsistierendes Produkt cy.js
@@ -1,6 +1,10 @@
 describe('nicht exsistierendes Produkt  ', () => {
   it('sollte nicht exsistierende Produkte anzeigen', () => {
 
+cy.visit('https://demowebshop.tricentis.com')
+cy.get('#small-searchterms').type('Autos{enter}');
+cy.get('.no-result').should('contain', 'No products were found');
+
 
 
 
